dc96fd072cd26aed22036ec4e77ff6b116abcb22
Pratik Tidke <pratiktidke987@gmail.com>
2026-05-30 19:02:59 +0530

    some changes in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import Home from './pages/Home'
+import Home from './components/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Room from './pages/Room'
@@ -8,7 +8,7 @@ import Profile from './pages/Profile'
 import UpdateUser from './pages/UpdateUser'
 import CreateRoom from './pages/CreateRoom'
 import Topics from './pages/Topics'
-import Activity from './pages/Activity'
+import { Activity } from 'react'
 
 function App() {
   return (
@@ -16,15 +16,15 @@ function App() {
       <div className="min-h-screen bg-den-bg text-den-text">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/room/:id" element={<Room />} />
-          <Route path="/profile/:id" element={<Profile />} />
-          <Route path="/update-user" element={<UpdateUser />} />
-          <Route path="/create-room" element={<CreateRoom />} />
-          <Route path="/topics" element={<Topics />} />
-          <Route path="/activity" element={<Activity />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/room/:id' element={<Room />} />
+          <Route path='/profile/:id' element={<Profile />} />
+          <Route path='/update-user' element={<UpdateUser />} />
+          <Route path='/create-room' element={<CreateRoom />} />
+          <Route path='/topics' element={<Topics />} />
+          <Route path='/activity' element={<Activity />} />
         </Routes>
       </div>
     </Router>
